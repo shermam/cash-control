1daf02056fb5a8ff3ed8a1799de37976092e1626
shermam.miranda <shermam.miranda@hotmail.com>
2018-01-03 13:48:46 -0200

    Separate table creation to new file

---
+++ public/js/createTransactionTable.js
@@ -0,0 +1,27 @@
+export function createTransactionsTable(transactions) {
+    const tableString = `
+        <table>
+            <thead>
+                <tr>
+                    <th>Data</th>
+                    <th>Descrição</th>
+                    <th>Valor</th>
+                    <th>Saldo</th>
+                </tr>
+            </thead>
+            <tbody>
+            ${transactions.map(t => {
+                return `
+                <tr>
+                    <td>${t.date}</td>
+                    <td>${t.description}</td>
+                    <td>${t.value || ''}</td>
+                    <td>${t.balance || ''}</td>
+                </tr>`
+            }).join('')}
+            </tbody >
+        </table >
+    `;
+
+    document.body.innerHTML += tableString;
+}
--- public/js/main.js
+++ public/js/main.js
@@ -3,16 +3,18 @@ import { extractData } from "./extractData.js";
 import { generateFileHTMLDoc } from "./generateFileHTMLDoc.js";
 import { flatten } from "./flatten.js";
 import { updateLabel } from "./updateLabel.js";
+import { createTransactionsTable } from "./createTransactionTable.js";
 
 document.querySelector('#file').addEventListener('change', async e => {
 
     updateLabel(e.target);
     const files = await readFiles(e);
-    const parsedFiles = files
+    const transactions = files
         .map(generateFileHTMLDoc)
         .map(extractData)
         .flatten();
 
-    console.log(parsedFiles);
+    console.log(transactions);
 
+    createTransactionsTable(transactions)
 });
