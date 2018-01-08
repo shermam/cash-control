afd9762938220fc098eeb34275b463d1f09c12b2
Shermam Miranda <shermam.miranda@hotmail.com>
2018-01-08 00:18:24 -0200

    adjustment to date formating to ignore timezone

--- public/js/formatters.js
+++ public/js/formatters.js
@@ -14,5 +14,6 @@ export function formatNumber(n) {
 
 
 export function formatDate(d) {
-    return new Date(d).toLocaleDateString();
+    return d.split('-').reverse().join('/');
+    //return new Date(d).toLocaleDateString();
 }
